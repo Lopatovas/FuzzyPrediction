1d41548af2de296beb89627f639e3b743416550b
Lopatovas <lopatovas@gmail.com>
2020-05-12 19:14:49 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='FuzzyPrediction',
+   version='1.0',
+   description='Tool for making predictions based on a set of inputs',
+   author='Edgaras Lopatovas',
+   author_email='lopatovas@gmail.com',
+   packages=['FuzzyPrediction'],
+)
